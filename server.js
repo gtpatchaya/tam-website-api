8266d7e548763426c8301af461360865574b7ccb
(nnot) patchaya <gtpatchaya@gmail.com>
2024-07-30 09:50:20 +0700

    fix : cors origin.

--- server.js
+++ server.js
@@ -4,26 +4,33 @@ const cors = require('cors');
 const categoryRoutes = require('./routes/categoryRoutes');
 const productRoutes = require('./routes/productRoutes');
 const authRoutes = require('./routes/authRoutes');
-const path = require('path')
+const path = require('path');
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
-
 app.use(express.json());
+app.use(cors({
+    origin: ['https://tam-website.netlify.app'],
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    credentials: true,
+    optionsSuccessStatus: 200 // for legacy browser support
+}));
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/api/categories', categoryRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/auth', authRoutes);
+
+app.options('*', cors({
+    origin: ['https://tam-website.netlify.app'],
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    credentials: true,
+    optionsSuccessStatus: 200 // for legacy browser support
+}));
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
