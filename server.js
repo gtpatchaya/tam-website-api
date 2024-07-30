74eaa8ca1ab4f968d0673034d39ee7f0cc6d34c6
(nnot) patchaya <gtpatchaya@gmail.com>
2024-07-30 10:15:17 +0700

    fix cors origin

--- server.js
+++ server.js
@@ -12,25 +12,20 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: ['https://tam-website.netlify.app'],
+    origin: 'https://tam-website.netlify.app',
     methods: ["GET", "POST", "PATCH", "DELETE"],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
     optionsSuccessStatus: 200 // for legacy browser support
 }));
-app.use(express.urlencoded({ extended: true }));
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
 app.use('/api/categories', categoryRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/auth', authRoutes);
-
-app.options('*', cors({
-    origin: ['https://tam-website.netlify.app'],
-    methods: ["GET", "POST", "PATCH", "DELETE"],
-    credentials: true,
-    optionsSuccessStatus: 200 // for legacy browser support
-}));
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
